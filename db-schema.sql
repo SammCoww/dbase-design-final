7e40c30572d44268263faf08125a6c1c078af3b7
Sam Cowan <cowansp@mail.uc.edu>
2026-07-30 10:17:54 -0400

    shouldve actually ran the SQL, pushing fixes

--- db-schema.sql
+++ db-schema.sql
@@ -1,12 +1,14 @@
-CREATE TABLE Customer ()
+CREATE TABLE Customer (
+    Customer_ID                 INT AUTO_INCREMENT PRIMARY KEY
+);
 CREATE TABLE Credit_Card(
     Credit_Card_Number          VARCHAR(19) PRIMARY KEY
-)
+);
 
 
 
 CREATE TABLE Product (
-    Product_ID                  INT AUTO_INCREMENT PRIMARY KEY
+    Product_ID                  INT AUTO_INCREMENT PRIMARY KEY,
     Product_Name                VARCHAR(150) NOT NULL,
     Price                       DECIMAL(10,2) NOT NULL,
     Stocked_Amount              INT NOT NULL DEFAULT 0
@@ -18,17 +20,16 @@ CREATE TABLE Purchase (
     CC_Number                   VARCHAR(19) NOT NULL,
     Purchase_Timestamp          DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     Purchase_Status             VARCHAR(30) NOT NULL DEFAULT 'Pending',
-    FOREIGN KEY (Customer_ID) REFERENCES Custmer(Customer_ID),
+    FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID),
     FOREIGN KEY (CC_Number) REFERENCES Credit_Card(Credit_Card_Number)
-)
+);
 
---Item(Weak entity)
 
 CREATE TABLE Item (
     Purchase_ID                 INT NOT NULL,
     Product_ID                  INT NOT NULL,
     Quantity                    INT NOT NULL DEFAULT 1,
     PRIMARY KEY (Purchase_ID, Product_ID),
-    FOREIGN KEY (Purchase_ID) REFERENCES Purchase(Purchase_ID)
+    FOREIGN KEY (Purchase_ID) REFERENCES Purchase(Purchase_ID),
     FOREIGN KEY (Product_ID) REFERENCES Product(Product_ID)
-)
+);
